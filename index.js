d858a5984261c300ca13cec252cb7ced133d1902
Eric Nograles <grales@gmail.com>
2017-10-10 11:00:01 -0400

    i feel pretty, oh so pretty

--- index.js
+++ index.js
@@ -8,66 +8,66 @@ const templateQuestions = require('./templateQuestions');
 const cwd = path.resolve(process.cwd());
 
 // Escape hatches below for exception files
-const validFiles = {'Dockerfile': true};
-const validExtensions = {'.js': true, '.json': true, '.yml': true, '.sh': true};
-const ignoreFiles = {'launch.json': true};
+const validFiles = { Dockerfile: true };
+const validExtensions = {
+  '.js': true,
+  '.json': true,
+  '.yml': true,
+  '.sh': true
+};
+const ignoreFiles = { 'launch.json': true };
 const _ = require('lodash');
 
 // Only watch for <%= %> swaps, lodash template swaps ES6 templates by default
 _.templateSettings.interpolate = /<%=([\s\S]+?)%>/g;
 
-function walkSync (dir, filelist) {
+function walkSync(dir, filelist) {
   let files = fs.readdirSync(dir);
   filelist = filelist || [];
-  files.forEach((file) => {
+  files.forEach(file => {
     if (fs.statSync(path.join(dir, file)).isDirectory()) {
       filelist = walkSync(path.join(dir, file), filelist);
-    }
-    else {
-      filelist.push({path: path.join(dir, file), name: file});
+    } else {
+      filelist.push({ path: path.join(dir, file), name: file });
     }
   });
   return filelist;
-};
+}
 
 function cloneRepo(url, projectName) {
-  return new Promise(
-    resolve => {
-      // Assumption: Any source repos will have a template branch that we can use
-      console.log(`Cloning ${url} to ./${projectName}`);
-      const clone = spawn('git', [`clone`, `-b`, `template`, url, projectName]);
-      clone.on('data', data => {
-        console.log(data);
-      });
+  return new Promise(resolve => {
+    // Assumption: Any source repos will have a template branch that we can use
+    console.log(`Cloning ${url} to ./${projectName}`);
+    const clone = spawn('git', [`clone`, `-b`, `template`, url, projectName]);
+    clone.on('data', data => {
+      console.log(data);
+    });
 
-      clone.on('close', code => {
-        console.log(`Finished cloning ${url} to ./${projectName}`);
-        resolve();
-      });
+    clone.on('close', code => {
+      console.log(`Finished cloning ${url} to ./${projectName}`);
+      resolve();
+    });
 
-      clone.on('error', error => {
-        throw (error);
-      })
-    }
-  );
+    clone.on('error', error => {
+      throw error;
+    });
+  });
 }
 
 function deleteGitFolder(projectName) {
-  return new Promise(
-    resolve => {
-      const pathToGit = path.join(cwd, `./${projectName}/.git`);
-      console.log(`Removing .git folder from ${pathToGit}`);
-      const del = spawn('rm', ['-rf', pathToGit]);
-      del.on('data', data => {
-        console.log(data);
-      });
+  return new Promise(resolve => {
+    const pathToGit = path.join(cwd, `./${projectName}/.git`);
+    console.log(`Removing .git folder from ${pathToGit}`);
+    const del = spawn('rm', ['-rf', pathToGit]);
+    del.on('data', data => {
+      console.log(data);
+    });
 
-      del.on('close', code => {
-        console.log(`Finished deleting .git folder`);
-        resolve();
-      });
-    }
-  );
+    del.on('close', code => {
+      console.log(`Finished deleting .git folder`);
+      resolve();
+    });
+  });
 }
 
 function readTemplateJson(projectName) {
@@ -92,7 +92,10 @@ function templateReplace(answers) {
   let files = walkSync(pathToProject);
   files.forEach(file => {
     let extension = path.extname(file.name);
-    if ((validExtensions[extension] || validFiles[file.name]) && !ignoreFiles[file.name]) {
+    if (
+      (validExtensions[extension] || validFiles[file.name]) &&
+      !ignoreFiles[file.name]
+    ) {
       console.log(`Swapping template values for ${file.path}...`);
       let fileTemplate = _.template(fs.readFileSync(file.path));
       fs.writeFileSync(file.path, fileTemplate(answers), { encoding: 'utf8' });
@@ -108,7 +111,12 @@ async function run() {
     let templateAnswers = await inquirer.prompt(
       askTemplateQuestions(readTemplateJson(answers.projectName).fields)
     );
-    templateReplace(Object.assign({...answers, ...templateAnswers}, {projectNameDocker: answers.projectName.replace(/-/g, '_')}));
+    templateReplace(
+      Object.assign(
+        { ...answers, ...templateAnswers },
+        { projectNameDocker: answers.projectName.replace(/-/g, '_') }
+      )
+    );
 
     // TODO: Read ".env" property from template.json, if it exists, to specify values for secrets
   } catch (error) {
