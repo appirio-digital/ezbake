529d3a78630a1a45bd787326a8f042f61728205c
Saurabh Deep <31232091+saurabh-deep@users.noreply.github.com>
2018-12-12 06:56:36 +0530

    fix: Space in system username issue caused ADX CLI to not execute (#35)

--- bin/ezbake.js
+++ bin/ezbake.js
@@ -8,6 +8,13 @@ const { debugLog } = require('../common');
 
 debugLog('System Node Version :: ', process.version);
 
+const fixWhiteSpace = input => {
+  if (input.indexOf(' ') > -1) {
+    return `"${input}"`;
+  }
+  return input;
+};
+
 const nodeExecPath = path.join(
   os.homedir(),
   '.appirio',
@@ -22,21 +29,28 @@ if (!fs.existsSync(nodeExec)) {
   process.stdout.write(
     '. Please wait while we install some missing EZBAKE dependencies...'
   );
-  const installScriptPath = path.join(__dirname, '..', 'install', 'node.js');
+  const installScriptPath = fixWhiteSpace(
+    path.join(__dirname, '..', 'install', 'node.js')
+  );
   execSync(`node ${installScriptPath} 8.12.0`);
   process.stdout.write(' Done!!\n');
 }
 
-const cliScriptFile = path.join(__dirname, 'index.js');
+const cliScriptFile = fixWhiteSpace(path.join(__dirname, 'index.js'));
 const { argv } = process;
 
 let args = [cliScriptFile];
 if (process.argv.length > 2) {
   argv.splice(0, 2);
+  argv.forEach((value, index) => {
+    if (typeof value === 'string' && value.indexOf(' ') > -1) {
+      argv[index] = fixWhiteSpace(value);
+    }
+  });
   args = args.concat(argv);
 }
 
-const cp = spawnSync(nodeExec, args, {
+const cp = spawnSync(fixWhiteSpace(nodeExec), args, {
   shell: true,
   stdio: 'inherit'
 });
